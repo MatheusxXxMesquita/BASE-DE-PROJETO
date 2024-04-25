bddc25dd7bc8eb255cc5bb7842ee55e6569bbfca
MatheuxXxMesquita <saydox45@gmail.com>
2024-04-25 12:07:19 -0300

    sabores de pizzas

--- src/Pages/sitePages/Home/index.jsx
+++ src/Pages/sitePages/Home/index.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  async function buscarSabores() {
+    const request = await fetch ("http://localhost:3000/sabores");
+    const response = await request.json();
+    setPizzas(response);
+  }
+
+  useEffect(() => {
+    buscarSabores();
+  }, []);
+
   return (
+  
   <>
-  Home
+  <h1>pizzas em destaque</h1>
+  {
+    pizzas && pizzas.map(p => (
+      <div>
+        {p.nome}
+      </div>
+    )) 
+  }
   </>
-  );
+  )
+  
 };
- export default Home;
+export default Home;
